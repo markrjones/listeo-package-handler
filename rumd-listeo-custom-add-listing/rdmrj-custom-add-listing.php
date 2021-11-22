83a959a0b9a5cb0db414cbd6bae1cf10c1884b8e
markrjones <mark_r_jones@live.co.uk>
2021-11-22 20:05:20 +0100

    Add files via upload

---
+++ rumd-listeo-custom-add-listing/rdmrj-custom-add-listing.php
@@ -0,0 +1,20 @@
+<?php
+   /*
+   Plugin Name: Listeo custom package handling for
+   Plugin URI: http://www.rumdoodle.at
+   description: Displays packages to an owner based on what category of listing they choose
+   Version: 0.1
+   Author: Mark Jones, Rum Doodle
+   Author URI: 
+   License: See Listeo licensing. This is their code with minor changes. Listeo was written
+            and is maintained by http://www.purethemes.net/
+   */
+
+  // The included file is a copy of class-listeo-core-submit.php which is part of the 
+  // listeo-core plugin. We've renamed the class so that we can modify it without
+  // interfering with the Listeo plugin. If Listeo changes the class we will need
+  // to change it here.
+
+  include( 'src/rdmrj-class-listeo-rdcustom-core-submit.php' );
+  $rdsubmit = Rdmrj_Custom_Listeo_Core_Submit::instance();
+?>
